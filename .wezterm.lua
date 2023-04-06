b18c8625508404260baef02fc9f256886ddbf5e5
domas-v <domas.vaitmonas93@gmail.com>
2023-04-06 21:37:20 +0300

    Update

--- .wezterm.lua
+++ .wezterm.lua
@@ -31,6 +31,7 @@ config.keys = {
     { key = ':', mods = 'CMD', action = act.ActivateCommandPalette},
     { key = 'X', mods = 'CMD', action = act.ActivateCopyMode },
     { key = 'N', mods = 'CMD', action = act.SpawnWindow },
+
     -- text navigation
     { key="F", mods="CTRL", action=wezterm.action{ SendString="\x1bf" } },
     { key="B", mods="CTRL", action=wezterm.action{ SendString="\x1bb" } },
@@ -42,29 +43,48 @@ config.keys = {
             wezterm.open_with(url)
         end)}
     },
+
     -- tab navigation
+    { key = 'A', mods = 'CMD', action = act.ShowTabNavigator },
     { key = 'T', mods = 'CMD', action = act.SpawnCommandInNewTab { cwd = "~", } },
+    { key = 'W', mods = 'CMD', action = act.CloseCurrentTab { confirm = true } },
+
     { key = 'n', mods = 'CMD', action = act.ActivateTabRelative(1) },
     { key = 'p', mods = 'CMD', action = act.ActivateTabRelative(-1) },
+
     { key = '[', mods = 'CMD', action = act.MoveTabRelative(-1) },
     { key = ']', mods = 'CMD', action = act.MoveTabRelative(1) },
-    { key = 'W', mods = 'CMD', action = act.CloseCurrentTab { confirm = true } },
+
     -- pane navigation
+    { key = 'S', mods = 'CMD', action = act.PaneSelect },
+    { key = 'm', mods = 'CMD', action = act.TogglePaneZoomState },
+    { key = 'w', mods = 'CMD', action = act.CloseCurrentPane { confirm = false } },
+
     { key = 'Enter', mods = 'CMD', action = act.SplitHorizontal },
     { key = 'Enter', mods = 'CMD|SHIFT', action = act.SplitHorizontal { cwd = "~" } },
+    { key = 'v', mods = 'CMD|CTRL', action = act.SplitPane {
+        direction = 'Right',
+        size = { Percent = 40 }, }
+    },
+
     { key = 'Enter', mods = 'CMD|CTRL', action = act.SplitVertical },
-    { key = 'v', mods = 'CMD|CTRL', action = act.SplitHorizontal },
-    { key = 's', mods = 'CMD|CTRL', action = act.SplitVertical },
+    { key = 's', mods = 'CMD|CTRL', action = act.SplitPane {
+        direction = 'Down',
+        size = { Percent = 30 }, }
+    },
+
     { key = 'h', mods = 'CMD', action = act.ActivatePaneDirection 'Left' },
     { key = 'j', mods = 'CMD', action = act.ActivatePaneDirection 'Down' },
     { key = 'k', mods = 'CMD', action = act.ActivatePaneDirection 'Up' },
     { key = 'l', mods = 'CMD', action = act.ActivatePaneDirection 'Right' },
-    { key = 'h', mods = 'LEADER', action = act.AdjustPaneSize { 'Left', 12 } },
-    { key = 'j', mods = 'LEADER', action = act.AdjustPaneSize { 'Down', 12 } },
-    { key = 'k', mods = 'LEADER', action = act.AdjustPaneSize { 'Up', 12 } },
-    { key = 'l', mods = 'LEADER', action = act.AdjustPaneSize { 'Right', 12 } },
-    { key = 'w', mods = 'CMD', action = act.CloseCurrentPane { confirm = false } },
-    { key = 'm', mods = 'CMD', action = act.TogglePaneZoomState },
+
+    { key = 'h', mods = 'CMD|CTRL', action = act.AdjustPaneSize { 'Left', 10 } },
+    { key = 'j', mods = 'CMD|CTRL', action = act.AdjustPaneSize { 'Down', 10 } },
+    { key = 'k', mods = 'CMD|CTRL', action = act.AdjustPaneSize { 'Up', 10 } },
+    { key = 'l', mods = 'CMD|CTRL', action = act.AdjustPaneSize { 'Right', 10 } },
+
+    { key = 'h', mods = 'CMD|SHIFT', action = act.RotatePanes 'Clockwise' },
+    { key = 'l', mods = 'CMD|SHIFT', action = act.RotatePanes 'CounterClockwise', },
 }
 
 return config
