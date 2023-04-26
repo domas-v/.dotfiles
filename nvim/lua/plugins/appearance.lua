91b04f266ca8e19ad5377704f496e82148fec266
domas-v <domas.vaitmonas93@gmail.com>
2023-04-26 18:53:29 +0300

    Borderless telescope

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -2,8 +2,31 @@ return {
     {
         "catppuccin/nvim",
         name = "catppuccin",
-        config = function()
+        config =
+        function()
+            local telescopeBorderless = function(flavor)
+                local cp = require("catppuccin.palettes").get_palette(flavor)
+                return {
+                    TelescopeBorder = { fg = cp.surface0, bg = cp.surface0 },
+                    TelescopeSelectionCaret = { fg = cp.flamingo, bg = cp.surface1 },
+                    TelescopeMatching = { fg = cp.peach },
+                    TelescopeNormal = { bg = cp.surface0 },
+                    TelescopeSelection = { fg = cp.text, bg = cp.surface1 },
+                    TelescopeMultiSelection = { fg = cp.text, bg = cp.surface2 },
+                    TelescopeTitle = { fg = cp.crust, bg = cp.green },
+                    TelescopePreviewTitle = { fg = cp.crust, bg = cp.red },
+                    TelescopePromptTitle = { fg = cp.crust, bg = cp.mauve },
+                    TelescopePromptNormal = { fg = cp.flamingo, bg = cp.crust },
+                    TelescopePromptBorder = { fg = cp.crust, bg = cp.crust },
+                }
+            end
             require("catppuccin").setup({
+                highlight_overrides = {
+                    latte = telescopeBorderless('latte'),
+                    frappe = telescopeBorderless('frappe'),
+                    macchiato = telescopeBorderless('macchiato'),
+                    mocha = telescopeBorderless('mocha'),
+                },
                 integrations = {
                     notify = true,
                     nvimtree = true,
