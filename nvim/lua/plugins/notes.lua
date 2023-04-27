c275d5d57ec35571129d6d5c9687dfc42fddaaf0
domas-v <domas.vaitmonas93@gmail.com>
2023-04-27 10:11:40 +0300

    Removed hologram

--- nvim/lua/plugins/notes.lua
+++ nvim/lua/plugins/notes.lua
@@ -13,6 +13,12 @@ return {
         }
     },
     -- {
+    --     "edluffy/hologram.nvim",
+    --     config = function()
+    --         require('hologram').setup({ auto_display = true })
+    --     end
+    -- }
+    -- {
     -- sniprun
     -- }
 }
