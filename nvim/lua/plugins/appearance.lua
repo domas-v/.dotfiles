484ec4e784a5a47d49d61572a0203e634aab7a84
domas-v <domas.vaitmonas93@gmail.com>
2023-05-04 08:28:56 +0300

    Update

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -3,76 +3,76 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         config =
-        function()
-            local telescopeBorderless = function(flavor)
-                local cp = require("catppuccin.palettes").get_palette(flavor)
-                return {
-                    TelescopeBorder = { fg = cp.surface0, bg = cp.surface0 },
-                    TelescopeSelectionCaret = { fg = cp.flamingo, bg = cp.surface1 },
-                    TelescopeMatching = { fg = cp.peach },
-                    TelescopeNormal = { bg = cp.surface0 },
-                    TelescopeSelection = { fg = cp.text, bg = cp.surface1 },
-                    TelescopeMultiSelection = { fg = cp.text, bg = cp.surface2 },
-                    TelescopeTitle = { fg = cp.crust, bg = cp.green },
-                    TelescopePreviewTitle = { fg = cp.crust, bg = cp.red },
-                    TelescopePromptTitle = { fg = cp.crust, bg = cp.mauve },
-                    TelescopePromptNormal = { fg = cp.flamingo, bg = cp.crust },
-                    TelescopePromptBorder = { fg = cp.crust, bg = cp.crust },
-                }
-            end
-            require("catppuccin").setup({
-                highlight_overrides = {
-                    latte = telescopeBorderless('latte'),
-                    frappe = telescopeBorderless('frappe'),
-                    macchiato = telescopeBorderless('macchiato'),
-                    mocha = telescopeBorderless('mocha'),
-                },
-                integrations = {
-                    notify = true,
-                    nvimtree = true,
-                    telescope = true,
-                    cmp = true,
-                    native_lsp = {
-                        enabled = true,
-                        virtual_text = {
-                            errors = { "bold" },
-                            hints = { "italic" },
-                            warnings = { "italic" },
-                            information = { "italic" },
+            function()
+                local telescopeBorderless = function(flavor)
+                    local cp = require("catppuccin.palettes").get_palette(flavor)
+                    return {
+                        TelescopeBorder = { fg = cp.surface0, bg = cp.surface0 },
+                        TelescopeSelectionCaret = { fg = cp.flamingo, bg = cp.surface1 },
+                        TelescopeMatching = { fg = cp.peach },
+                        TelescopeNormal = { bg = cp.surface0 },
+                        TelescopeSelection = { fg = cp.text, bg = cp.surface1 },
+                        TelescopeMultiSelection = { fg = cp.text, bg = cp.surface2 },
+                        TelescopeTitle = { fg = cp.crust, bg = cp.green },
+                        TelescopePreviewTitle = { fg = cp.crust, bg = cp.red },
+                        TelescopePromptTitle = { fg = cp.crust, bg = cp.mauve },
+                        TelescopePromptNormal = { fg = cp.flamingo, bg = cp.crust },
+                        TelescopePromptBorder = { fg = cp.crust, bg = cp.crust },
+                    }
+                end
+                require("catppuccin").setup({
+                    highlight_overrides = {
+                        latte = telescopeBorderless('latte'),
+                        frappe = telescopeBorderless('frappe'),
+                        macchiato = telescopeBorderless('macchiato'),
+                        mocha = telescopeBorderless('mocha'),
+                    },
+                    integrations = {
+                        notify = true,
+                        nvimtree = true,
+                        telescope = true,
+                        cmp = true,
+                        native_lsp = {
+                            enabled = true,
+                            virtual_text = {
+                                errors = { "bold" },
+                                hints = { "italic" },
+                                warnings = { "italic" },
+                                information = { "italic" },
+                            },
+                            underlines = {
+                                errors = { "undercurl" },
+                                hints = { "undercurl" },
+                                warnings = { "undercurl" },
+                                information = { "undercurl" },
+                            }
                         },
-                        underlines = {
-                            errors = { "undercurl" },
-                            hints = { "undercurl" },
-                            warnings = { "undercurl" },
-                            information = { "undercurl" },
-                        }
-                    },
-                    dap = {
-                        enabled = true,
-                        enable_ui = true,
-                    },
-                    mason = true,
-                    lsp_trouble = true,
-                    symbols_outline = true,
-                    treesitter = true,
-                    neogit = true,
-                    gitsigns = true,
-                    barbar = true,
-                    leap = true,
-                    illuminate = true,
-                    indent_blankline = {
-                        enabled = true,
-                        colored_indent_levels = false,
-                    },
-                    markdown = true,
-                    headlines = true
-                }
-            })
-        end,
+                        dap = {
+                            enabled = true,
+                            enable_ui = true,
+                        },
+                        mason = true,
+                        lsp_trouble = true,
+                        symbols_outline = true,
+                        treesitter = true,
+                        neogit = true,
+                        gitsigns = true,
+                        barbar = true,
+                        leap = true,
+                        illuminate = true,
+                        indent_blankline = {
+                            enabled = true,
+                            colored_indent_levels = false,
+                        },
+                        markdown = true,
+                        headlines = true
+                    }
+                })
+            end,
     },
     {
         "f-person/auto-dark-mode.nvim",
-        config = function ()
+        config = function()
             local auto_dark_mode = require('auto-dark-mode')
 
             auto_dark_mode.setup({
