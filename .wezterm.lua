043f4f387a34301a6acde79cbff4e55addb4e498
domas-v <domas.vaitmonas93@gmail.com>
2023-09-26 21:40:26 +0300

    wezterm tweaks

--- .wezterm.lua
+++ .wezterm.lua
@@ -48,43 +48,6 @@ local basename = function(s)
     return string.gsub(s, "(.*[/\\])(.*)", "%2")
 end
 
-wezterm.on("update-status", function(window, pane)
-    local stat = window:active_workspace()
-    local stat_color = "#f7768e"
-
-    if window:active_key_table() then
-        stat = window:active_key_table()
-        stat_color = "#7dcfff"
-    end
-    if window:leader_is_active() then
-        stat = "LDR"
-        stat_color = "#bb9af7"
-    end
-
-    -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l). Not a big deal, but check in case
-    local cwd = pane:get_current_working_dir()
-    cwd = cwd and basename(cwd) or ""
-    -- Current command
-    local cmd = pane:get_foreground_process_name()
-    cmd = cmd and basename(cmd) or ""
-
-    -- window:set_left_status(wezterm.format({
-    --     { Foreground = { Color = stat_color } },
-    --     { Text = "  " },
-    --     { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
-    --     { Text = " |" },
-    -- }))
-    -- window:set_right_status(wezterm.format({
-    --     { Foreground = { Color = stat_color } },
-    --     { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
-    --     { Text = " | " },
-    --     { Foreground = { Color = "#e0af68" } },
-    --     { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
-    --     "ResetAttributes",
-    --     { Text = "  " },
-    -- }))
-end)
-
 wezterm.on(
     'format-tab-title',
     function(tab, tabs, panes, config, hover, max_width)
