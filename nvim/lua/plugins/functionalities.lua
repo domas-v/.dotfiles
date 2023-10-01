dcb05a5afdc7387f6dab2ffb4c75a4878eb58acc
domas-v <domas.vaitmonas93@gmail.com>
2023-10-01 10:41:18 +0300

    iron --> vim-slime

--- nvim/lua/plugins/functionalities.lua
+++ nvim/lua/plugins/functionalities.lua
@@ -1,55 +1,5 @@
 return {
     {
-        "hkupty/iron.nvim",
-        config = function()
-            local iron = require("iron.core")
-            local view = require("iron.view")
-
-            iron.setup {
-                config = {
-                    scratch_repl = false,
-                    repl_open_cmd = view.split.vertical.botright(0.5)
-                },
-                repl_definition = {
-                    scheme = {
-                        command = { "racket", "-i" }
-                    },
-                    racket = {
-                        command = { "racket", "-i" }
-                    },
-                    guile = {
-                        command = { "racket", "-i" }
-                    },
-                    python = {
-                        command = { "ipython" }
-                    },
-                },
-                keymaps = {
-                    send_file = "<space>if",
-                    send_motion = "<space>im",
-                    visual_send = "<space><cr>",
-                    send_line = "<space><cr>",
-                    clear = "<space>il",
-                    interrupt = "<space>ix",
-                    exit = "<space>iq",
-                    -- cr = "<space><cr>",
-                },
-                highlight = {
-                    italic = true
-                },
-                ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
-            }
-        end,
-        keys = {
-            { "<leader>ii", "<cmd>IronRepl<cr>" },
-            { "<leader>ih", "<cmd>IronReplHere<cr>" },
-        },
-        cmd = {
-            "IronRepl",
-            "IronReplHere",
-        }
-    },
-    {
         'rmagatti/auto-session',
         config = function()
             require("auto-session").setup {
@@ -60,5 +10,16 @@ return {
     },
     {
         "knubie/vim-kitty-navigator"
+    },
+    {
+        "jpalardy/vim-slime",
+        init = function()
+            vim.g.slime_target = "kitty"
+            -- vim.g.slime_python_ipython = 1
+            -- vim.g.slime_dont_ask_default = 1
+        end,
+        keys = {
+            { "<C-c><C-f>", "<cmd>%SlimeSend<cr>", "Slime send file" },
+        }
     }
 }
