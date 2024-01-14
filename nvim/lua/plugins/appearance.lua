5db9bd6019a6e780975eed63b98ddc2cf1447117
domas-v <domas.vaitmonas93@gmail.com>
2024-01-14 22:39:04 +0200

    Tokyo Night Storm

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -1,24 +1,22 @@
 return {
     {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            require("kanagawa").setup({
-                transparent = true,
-                colors = {
-                    theme = {
-                        all = {
-                            ui = {
-                                bg_gutter = "none"
-                            }
-                        }
-                    }
-                }
-            })
-        end
-    },
-    {
          "catppuccin/nvim",
          name = "catppuccin",
          opts = { transparent_background = true }
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent = true,
+            styles = {
+                comments = { italic = true },
+                keywords = { italic = true },
+                functions = { italic = true },
+                variables = { italic = true }, 
+            },
+        }
     }
+
 }
