7c138c86dab408887346c47f012be76bf42e334a
domas-v <domas.vaitmonas93@gmail.com>
2024-04-21 22:21:46 +0300

    Update

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -42,16 +42,31 @@ return {
         end
     },
     {
-        -- remembers which files were open
-        "EricDriussi/remember-me.nvim",
-        config = function()
-            require("remember_me").setup({
-                ignore_ft = { "man", "gitignore", "gitcommit" },
-                session_store = "~/.cache/remember-me/",
-                ext = ".r.vim",
-                project_roots = { ".git", ".svn" },
-            })
-        end
+        -- -- remembers which files were open
+        -- "EricDriussi/remember-me.nvim",
+        -- config = function()
+        --     require("remember_me").setup({
+        --         ignore_ft = { "man", "gitignore", "gitcommit" },
+        --         session_store = "~/.cache/remember-me/",
+        --         ext = ".r.vim",
+        --         project_roots = { ".git", ".svn" },
+        --     })
+        -- end
     },
     { "RRethy/vim-illuminate" },
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1001, -- this plugin needs to run before anything else
+        opts = {
+            rocks = { "magick" },
+        },
+    },
+    {
+        "3rd/image.nvim",
+        dependencies = { "luarocks.nvim" },
+        config = function()
+            require("image").setup()
+        end
+    }
+
 }
