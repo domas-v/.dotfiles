a454afc2a74fee8d28dd2ff8daaa1258055b9fab
domas-v <domas.vaitmonas93@gmail.com>
2024-08-20 22:34:08 +0300

    Removed telescope and github theme

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -1,22 +1,5 @@
 return {
     {
-        "projekt0n/github-nvim-theme",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            require('github-theme').setup({
-                options = {
-                    transparent = true,
-                    styles = {
-                        comments = 'italic',
-                        keywords = 'bold',
-                        types = 'italic,bold',
-                    }
-                }
-            })
-        end
-    },
-    {
         "rebelot/kanagawa.nvim",
         lazy = false,
         priority = 1000,
