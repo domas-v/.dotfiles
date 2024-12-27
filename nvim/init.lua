39de9dac797f22761c79469a14014df9e3b1f12a
domas-v <domas.vaitmonas93@gmail.com>
2024-12-27 10:22:04 +0200

    Theme based on system dark/light mode

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,25 @@
 require("settings")
-require("remap")
+require("keybindings")
 require("config.lazy")
 
-vim.cmd("colorscheme kanagawa-dragon")
+
+local function get_system_theme_mac()
+    -- Returns `Dark` if it's dark, otherwise doesn't return anything
+    local handle = io.popen('defaults read -g AppleInterfaceStyle 2>/dev/null')
+    if handle then
+        local result = handle:read("*a")
+        handle:close()
+        return result:match("Dark")
+    end
+end
+
+if get_system_theme_mac() then
+    vim.o.background = "dark"
+    vim.cmd("colorscheme kanagawa-wave")
+else
+    vim.o.background = "light"
+    vim.cmd("colorscheme kanagawa-lotus")
+end
 
 -- htto filetype is not detected somehow. this is a fix
 vim.filetype.add({
