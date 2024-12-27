ea03fe620c60dfacab1107e0f6ba578640d75f62
domas-v <domas.vaitmonas93@gmail.com>
2024-12-27 10:22:27 +0200

    Unnecessary completion keybindings removed

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -11,11 +11,6 @@ return {
                 ["<TAB>"] = { "select_and_accept" },
                 ["<C-j>"] = { "select_next" },
                 ["<C-k>"] = { "select_prev" },
-                cmdline = {
-                    ["<TAB>"] = { "select_and_accept" },
-                    ["<C-j>"] = { "select_next" },
-                    ["<C-k>"] = { "select_prev" },
-                }
             },
             appearance = {
                 use_nvim_cmp_as_default = true,
