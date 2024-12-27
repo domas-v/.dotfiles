3db5061650b5e76b92c4f46e56b41e88a1b8d3d4
domas-v <domas.vaitmonas93@gmail.com>
2024-12-27 22:39:22 +0200

    Update

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,3 +1,6 @@
+local function is_dap_buffer()
+    return require("cmp_dap").is_dap_buffer()
+end
 return {
     {
         'saghen/blink.compat',
@@ -7,7 +10,7 @@ return {
     },
     {
         "saghen/blink.cmp",
-        enabled = false,
+        enabled = true,
         dependencies = { "rafamadriz/friendly-snippets", "rcarriga/cmp-dap" },
         version = "*",
         opts = {
@@ -20,18 +23,28 @@ return {
                     ["<CR>"] = { "accept", "fallback" },
                     ["<C-j>"] = { "select_next" },
                     ["<C-k>"] = { "select_prev" },
+                    ["<C-n>"] = { "select_next" },
+                    ["<C-p>"] = { "select_prev" },
                     ["<TAB>"] = { "select_and_accept" }
                 }
             },
+            enabled = function()
+                return vim.bo.buftype ~= "prompt" or is_dap_buffer()
+            end,
             sources = {
-                default = { "lsp", "path", "snippets", "buffer", "dadbod", "dap" },
+                default = function(_)
+                    local sql_filetypes = { mysql = true, sql = true }
+                    if sql_filetypes[vim.bo.filetype] ~= nil then
+                        return { "dadbod", "snippets", "buffer" }
+                    elseif is_dap_buffer() then
+                        return { "dap", "snippets", "buffer" }
+                    else
+                        return { "lsp", "path", "snippets", "buffer" }
+                    end
+                end,
                 providers = {
                     dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-                    dap = {
-                        name = "cmp-dap",
-                        module = "blink.compat.source",
-                        enabled = true,
-                    },
+                    dap = { name = "dap", module = "blink.compat.source" },
                 },
             },
             completion = {
@@ -45,7 +58,7 @@ return {
     },
     {
         "hrsh7th/nvim-cmp",
-        enabled = true,
+        enabled = false,
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-nvim-lua",
