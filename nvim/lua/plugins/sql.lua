67ae4bdd08ad811ab94c2e02657e213806bb32cc
domas-v <domas.vaitmonas93@gmail.com>
2024-12-28 23:31:56 +0200

    Cosmetic change

--- nvim/lua/plugins/sql.lua
+++ nvim/lua/plugins/sql.lua
@@ -2,7 +2,7 @@ return {
     'kristijanhusak/vim-dadbod-ui',
     dependencies = {
         { 'tpope/vim-dadbod',                     lazy = true },
-        { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+        { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' } }
     },
     cmd = {
         'DBUI',
@@ -15,4 +15,3 @@ return {
         vim.keymap.set("n", "<leader><leader>d", ":DBUIToggle<cr>", { noremap = true, silent = true })
     end,
 }
-
