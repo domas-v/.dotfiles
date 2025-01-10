d2f3a96f97daec4a338f32c0fa53084dc204c0d6
Domas Vaitmonas <domas.vaitmonas93@gmail.com>
2025-01-10 10:37:21 +0200

    Disable sessions

--- nvim/lua/plugins/sessions.lua
+++ nvim/lua/plugins/sessions.lua
@@ -1,6 +1,7 @@
 return {
     {
         'rmagatti/auto-session',
+        enabled = false,
         lazy = false,
         opts = {
             suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
