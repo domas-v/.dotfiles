42a2499fcd076a531d90f5c16f74a1ef740bb2d0
Domas Vaitmonas <domas.vaitmonas93@gmail.com>
2025-03-21 18:12:22 +0200

    Removed not needed colorschemes

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -23,43 +23,5 @@ return {
                 },
             })
         end
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        enabled = false,
-        lazy = false,
-        priority = 1000,
-        opts = {
-            transparent = false,
-            colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
-            overrides = function(colors)
-                local theme = colors.theme
-                return {
-                    NormalFloat = { bg = "none" },
-                    FloatBorder = { bg = "none" },
-                    FloatTitle = { bg = "none" },
-                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-                    LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                }
-            end,
-
-        }
-    },
-    {
-        "folke/tokyonight.nvim",
-        enabled = false,
-        lazy = false,
-        priority = 1000,
-        opts = {
-            transparent = true,
-            styles = {
-                comments = { italic = true },
-                keywords = { bold = true },
-                functions = { bold = true },
-                variables = {},
-                sidebars = "dark",
-                floats = "dark",
-            },
-        },
-    },
+    }
 }
