59f7b9f732cf0cbed589a959f77a3e5bdd58d008
Domas Vaitmonas <domas.vaitmonas93@gmail.com>
2025-03-21 18:11:55 +0200

    init.lua cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,27 +2,7 @@ require("settings")
 require("keybindings")
 require("config.lazy")
 
-local function get_system_theme_mac()
-    -- Returns `Dark` if it's dark, otherwise doesn't return anything
-    local handle = io.popen('defaults read -g AppleInterfaceStyle 2>/dev/null')
-    if handle then
-        local result = handle:read("*a")
-        handle:close()
-        return result:match("Dark")
-    end
-end
-
 vim.cmd("colorscheme catppuccin")
--- if get_system_theme_mac() then
---     vim.o.background = "dark"
--- else
---     vim.o.background = "light"
---     vim.cmd("colorscheme catppuccin-latte")
--- end
 
 -- htto filetype is not detected somehow. this is a fix
-vim.filetype.add({
-    extension = {
-        http = "http",
-    }
-})
+vim.filetype.add({ extension = { http = "http" } })
