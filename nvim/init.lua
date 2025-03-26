bb621b1a7a70ae829775052ef3a00e7f9029f0fb
Domas Vaitmonas <domas.vaitmonas93@gmail.com>
2025-03-26 08:33:52 +0200

    New colorschemes

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ require("settings")
 require("keybindings")
 require("config.lazy")
 
-vim.cmd("colorscheme catppuccin-frappe")
+vim.cmd("colorscheme kanagawa")
 
 -- HACK: http filetype is not detected somehow. this is a fix
 vim.filetype.add({ extension = { http = "http" } })
--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -23,5 +23,31 @@ return {
                 },
             })
         end
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine"
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+        init = function()
+            vim.api.nvim_create_autocmd("ColorScheme", {
+                pattern = "kanagawa",
+                callback = function()
+                    vim.api.nvim_set_hl(0, "StatusLine", { link = "lualine_c_normal" })
+                end,
+            })
+        end,
+        opts = {
+            colors = { theme = { all = { ui = { bg_gutter = "none" } } } }
+        },
     }
 }
