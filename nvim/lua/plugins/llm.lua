6752bdf8fd95d30ca8ce30b44c1beec30bdff428
Domas Vaitmonas <domas.vaitmonas93@gmail.com>
2025-05-23 13:38:25 +0300

    update

--- nvim/lua/plugins/llm.lua
+++ nvim/lua/plugins/llm.lua
@@ -15,5 +15,26 @@ return {
             "nvim-lua/plenary.nvim",
             "nvim-treesitter/nvim-treesitter",
         },
-    }
+    },
+    {
+        'milanglacier/minuet-ai.nvim',
+        config = function()
+            require('minuet').setup {
+                provider = "claude",
+                throttle = 200,
+                debounce = 100,
+                virtualtext = {
+                    auto_trigger_ft = {},
+                    keymap = {
+                        accept = '<C-e>',
+                        accept_line = '<A-l>',
+                        -- accept_n_lines = '<C-e>',
+                        prev = '<A-[>',
+                        next = '<A-]>',
+                        dismiss = '<A-k>',
+                    },
+                }
+            }
+        end,
+    },
 }
