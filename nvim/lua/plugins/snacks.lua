3d3bdac85643c91d503f136e454efdaad78a5991
Domas Vaitmonas <domas.vaitmonas93@gmail.com>
2025-10-27 16:00:49 -0500

    Update

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -53,7 +53,7 @@ return {
                     filename_bonus = true,
                     file_pos = true,
                     cwd_bonus = true,
-                    frecency = false,
+                    frecency = true,
                     history_bonus = true,
                 },
                 sources = {
