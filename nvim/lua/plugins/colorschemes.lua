1c176ebd782c77a9195775bd588ea2360673c1f6
Domas Vaitmonas <domas.vaitmonas93@gmail.com>
2025-11-22 17:28:58 +0200

    Removed unused colorschemes

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,59 +1,8 @@
 return {
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        lazy = false,
-        opts = {
-            styles = { transparency = false }
-        }
-    },
-    {
-        "catppuccin/nvim",
-        enabled = false,
-        lazy = false,
-        priority = 1000,
-        name = "catppuccin",
-        config = function()
-            require("catppuccin").setup({
-                transparent_background = false,
-                styles = {
-                    comments = { "italic" },
-                    conditionals = { "italic" },
-                    loops = { "italic" },
-                    functions = { "italic" },
-                    keywords = { "bold" },
-                },
-                default_integrations = true,
-                integrations = {
-                    gitsigns = true,
-                    treesitter = true,
-                    blink_cmp = true,
-                    diffview = true,
-                    nvim_surround = true,
-                },
-            })
-        end
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        enabled = false,
-        lazy = false,
-        priority = 1000,
-        init = function()
-            vim.api.nvim_create_autocmd("ColorScheme", {
-                pattern = "kanagawa",
-                callback = function()
-                    vim.api.nvim_set_hl(0, "StatusLine", { link = "lualine_c_normal" })
-                end,
-            })
-        end,
-        opts = {
-            colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
-            transparent = true
-        },
-    },
-    {
-        "f-person/auto-dark-mode.nvim",
-        opts = {}
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    opts = {
+        styles = { transparency = false }
     }
 }
