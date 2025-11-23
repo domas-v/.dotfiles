a10270132439f64ceb5e4fc8ebc35ea08ff3edff
Domas Vaitmonas <domas.vaitmonas93@gmail.com>
2025-11-23 12:22:15 +0200

    quickfix plugin in other file

--- nvim/lua/plugins/buffers.lua
+++ nvim/lua/plugins/buffers.lua
@@ -12,14 +12,6 @@ return {
         }
     },
     {
-        "kevinhwang91/nvim-bqf",
-        ft = 'qf',
-        dependencies = {
-            'junegunn/fzf',
-            config = function() vim.fn['fzf#install']() end
-        }
-    },
-    {
         'akinsho/bufferline.nvim',
         lazy = false,
         version = "*",
