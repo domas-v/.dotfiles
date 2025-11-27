3c8099e1d6d4a07d690216da902a7b7b9a5d3aaa
Domas Vaitmonas <domas.vaitmonas93@gmail.com>
2025-11-27 08:01:25 +0200

    Update

--- nvim/lua/plugins/windows.lua
+++ nvim/lua/plugins/windows.lua
@@ -1,14 +1,6 @@
 return {
-    {
-        "utilyre/barbecue.nvim",
-        name = "barbecue",
-        version = "*",
-        dependencies = {
-            "SmiteshP/nvim-navic",
-            "nvim-tree/nvim-web-devicons",
-        },
-        opts = {},
-    },
+
+    -- WINDOW NAVIGATION
     {
         "kevinhwang91/nvim-bqf",
         ft = 'qf',
@@ -46,5 +38,81 @@ return {
             { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
             { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
         },
-    }
+    },
+    {
+        "vladdoster/remember.nvim", -- remembers where in file I was
+        config = function()
+            require('remember')
+        end
+    },
+
+    -- WINDOW DECORATIONS
+    {
+        "utilyre/barbecue.nvim",
+        name = "barbecue",
+        version = "*",
+        dependencies = {
+            "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {},
+    },
+    {
+        "chrisgrieser/nvim-origami",
+        event = "VeryLazy",
+        version = "1.9",
+        opts = { setupFoldKeymaps = false, },
+    },
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = { "kevinhwang91/promise-async" },
+        config = function()
+            vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+            vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+            require('ufo').setup({
+                provider_selector = function(_, _, _)
+                    return { 'treesitter', 'indent' }
+                end,
+                enable_get_fold_virt_text = true,
+                open_fold_hl_timeout = 150,
+                preview = {
+                    win_config = {
+                        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+                        winhighlight = "Normal:Folded",
+                        winblend = 0,
+                    },
+                    mappings = {
+                        jumpTop = "[",
+                        jumpBot = "]",
+                    },
+                },
+            })
+        end
+    },
+
+    -- TEXT NAVIGATION
+    {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup() end,
+        event = "InsertEnter",
+    },
+    {
+        "kylechui/nvim-surround",
+        config = function() require("nvim-surround").setup({ move_cursor = false }) end,
+        event = "InsertEnter",
+    },
+    {
+        "numToStr/Comment.nvim",
+        config = function() require("Comment").setup() end
+    },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = { modes = { search = { enabled = false } } },
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,                                       desc = "Flash" },
+            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,                                 desc = "Flash Treesitter" },
+            { "W", mode = { "n", "x", "o" }, function() require("flash").jump({ pattern = vim.fn.expand("<cword>") }) end, desc = "Flash Treesitter" },
+        },
+    },
 }
