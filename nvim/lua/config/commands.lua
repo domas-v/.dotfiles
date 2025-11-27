40ca2e7b510f3c180b139981b0730c7bdd40a717
Domas Vaitmonas <domas.vaitmonas93@gmail.com>
2025-11-27 19:25:17 +0200

    Renamed

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -8,3 +8,10 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = "qf", -- in quickfix list
     callback = function() vim.keymap.set("n", "q", "<cmd>cclose<cr>", { buffer = true, silent = true }) end
 })
+
+
+-- opens in system file explorer
+vim.api.nvim_create_user_command("OpenDir", function()
+    local dir = vim.fn.expand('%:p:h')
+    os.execute('open ' .. vim.fn.shellescape(dir))
+end, { desc = "Open current file's directory in system file explorer" })
