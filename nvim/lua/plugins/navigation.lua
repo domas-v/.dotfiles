aefb1fdfe10218e5db510f834d956a28a5d9067c
Domas Vaitmonas <domas.vaitmonas93@gmail.com>
2025-11-27 08:02:40 +0200

    Rename

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -57,38 +57,6 @@ return {
         },
         opts = {},
     },
-    {
-        "chrisgrieser/nvim-origami",
-        event = "VeryLazy",
-        version = "1.9",
-        opts = { setupFoldKeymaps = false, },
-    },
-    {
-        "kevinhwang91/nvim-ufo",
-        dependencies = { "kevinhwang91/promise-async" },
-        config = function()
-            vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-            vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-            require('ufo').setup({
-                provider_selector = function(_, _, _)
-                    return { 'treesitter', 'indent' }
-                end,
-                enable_get_fold_virt_text = true,
-                open_fold_hl_timeout = 150,
-                preview = {
-                    win_config = {
-                        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-                        winhighlight = "Normal:Folded",
-                        winblend = 0,
-                    },
-                    mappings = {
-                        jumpTop = "[",
-                        jumpBot = "]",
-                    },
-                },
-            })
-        end
-    },
 
     -- TEXT NAVIGATION
     {
