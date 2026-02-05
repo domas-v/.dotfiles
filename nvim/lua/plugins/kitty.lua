9e88fd65792457f1a688d6db5858ffe05028468f
Dom <domas.vaitmonas93@gmail.com>
2026-02-05 08:29:17 +0200

    Kitty navigator

--- nvim/lua/plugins/kitty.lua
+++ nvim/lua/plugins/kitty.lua
@@ -1,8 +1,19 @@
 return {
-    "MunsMan/kitty-navigator.nvim",
-    build = {
-        "cp navigate_kitty.py ~/.config/kitty",
-        "cp pass_keys.py ~/.config/kitty",
+    {
+        "MunsMan/kitty-navigator.nvim",
+        build = {
+            "cp navigate_kitty.py ~/.config/kitty",
+            "cp pass_keys.py ~/.config/kitty",
+        },
+        opts = { keybindings = {} },
     },
-    opts = { keybindings = {} },
+    {
+        'mikesmithgh/kitty-scrollback.nvim',
+        lazy = true,
+        cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
+        event = { 'User KittyScrollbackLaunch' },
+        config = function()
+            require('kitty-scrollback').setup()
+        end,
+    }
 }
