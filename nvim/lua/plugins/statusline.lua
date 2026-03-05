068a9af40a356f183d270fd0a994324f077eeaba
Dom <domas.vaitmonas93@gmail.com>
2026-03-05 16:06:27 +0200

    Lualine

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -1,18 +1,15 @@
 return {
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        opts = {
-            sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { 'branch', 'diff', },
-                lualine_c = { 'filename' },
-                lualine_x = { 'filetype' },
-                lualine_y = { 'location' },
-                lualine_z = { '' }
-            },
-            component_separators = { left = require("config.icons").arrows.left_solid, right = require("config.icons").arrows.right_solid },
-            section_separators = { left = require("config.icons").arrows.left_solid, right = require("config.icons").arrows.right_solid },
-        },
-    }
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+        options = { globalstatus = true, },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff', },
+            lualine_c = { 'filename' },
+            lualine_x = { 'filetype' },
+            lualine_y = { 'location' },
+            lualine_z = { '' }
+        }
+    },
 }
