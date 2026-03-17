00cb694c369885fde35029cc68d881f9639b1047
Dom <domas.vaitmonas93@gmail.com>
2026-03-17 12:02:13 +0200

    Markdown highlights

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,6 +18,26 @@ vim.api.nvim_create_autocmd('FileType', {
     end
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function()
+        local function badge_hl(name, source)
+            local hl = vim.api.nvim_get_hl(0, { name = source, link = false })
+            vim.api.nvim_set_hl(0, name, { bg = hl.fg, fg = hl.bg or "#1e1e2e", bold = true })
+        end
+
+        badge_hl("MarkdownTodo", "DiagnosticWarn")
+        badge_hl("MarkdownDoing", "DiagnosticInfo")
+        badge_hl("MarkdownWaiting", "DiagnosticOK")
+        badge_hl("MarkdownImportant", "DiagnosticError")
+
+        vim.fn.matchadd("MarkdownTodo", "TODO")
+        vim.fn.matchadd("MarkdownDoing", "DOING")
+        vim.fn.matchadd("MarkdownWaiting", "WAITING")
+        vim.fn.matchadd("MarkdownImportant", "IMPORTANT")
+    end,
+})
+
 -- Enable completion for DAP-REPL filetypes for blink.cmp
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "dap-repl", "dapui_watches", "dapui_hover" },
@@ -26,4 +46,3 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
     desc = "Enable completion for DAP-REPL filetypes"
 })
-
