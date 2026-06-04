9c25f976bad2a9de1564753a5523ac7c3b5c60ae
Dom <domas.vaitmonas93@gmail.com>
2026-06-04 20:21:46 +0300

    Neogit keybinds and opts

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -59,14 +59,21 @@ return {
         lazy = true,
         dependencies = { "esmuellert/codediff.nvim" },
         cmd = "Neogit",
+        opts = {
+            commit_editor = {
+                kind = "vsplit",
+                show_staged_diff = true,
+            },
+        },
         keys = {
             { "<leader>gg", "<cmd>Neogit kind=auto<cr>" },
             { "<leader>gv", "<cmd>Neogit kind=vsplit<cr>" },
             { "<leader>G",  "<cmd>Neogit kind=tab<cr>" },
-            { "<leader>gp", "<cmd>Neogit pull" },
-            { "<leader>gP", "<cmd>Neogit push" },
-            { "<leader>gF", "<cmd>Neogit fetch" },
-        }
+            { "<leader>gp", "<cmd>Neogit pull<cr>" },
+            { "<leader>gP", "<cmd>Neogit push<cr>" },
+            { "<leader>gF", "<cmd>Neogit fetch<cr>" },
+            { "<leader>gc", "<cmd>Neogit commit<cr>" },
+        },
     },
     {
         "lewis6991/gitsigns.nvim",
