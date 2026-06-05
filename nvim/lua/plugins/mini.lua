8072ebfb6e5abfd239c4b28d06787270cc4baabb
Dom <domas.vaitmonas93@gmail.com>
2026-06-05 17:09:29 +0300

    Added overlay to mini.diff

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -33,16 +33,23 @@ return {
     {
         'nvim-mini/mini.diff',
         version = false,
-        opts = {
-            mappings = {
-                apply = 'ga',
-                reset = 'gA',
-                goto_first = '[G',
-                goto_prev = '[g',
-                goto_next = ']g',
-                goto_last = ']G',
-            }
-        }
+        config = function()
+            local diff = require('mini.diff')
+            diff.setup(
+                {
+                    mappings = {
+                        apply = 'ga',
+                        reset = 'gA',
+                        goto_first = '[G',
+                        goto_prev = '[g',
+                        goto_next = ']g',
+                        goto_last = ']G',
+                    }
+
+                }
+            )
+            vim.keymap.set("n", "gt", "<cmd>lua MiniDiff.toggle_overlay()<cr>")
+        end
     },
     {
         'nvim-mini/mini-git',
