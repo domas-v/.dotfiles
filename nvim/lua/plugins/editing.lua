a84378d04ea07f73b651b1a564d52804e9911f31
Dom <domas.vaitmonas93@gmail.com>
2026-06-07 22:05:17 +0300

    Restore commenting plugin

---
+++ nvim/lua/plugins/editing.lua
@@ -0,0 +1,38 @@
+return {
+    {
+        "justinmk/vim-sneak",
+        lazy = false,
+        init = function()
+            vim.g["sneak#label"] = 1
+            vim.g["sneak#s_next"] = 1
+        end,
+        config = function()
+            vim.api.nvim_set_hl(0, "Sneak", { link = "IncSearch" })
+
+            vim.keymap.set('n', 'f', "<Plug>Sneak_f", { noremap = true })
+            vim.keymap.set('n', 'F', "<Plug>Sneak_F", { noremap = true })
+            vim.keymap.set('n', 't', "<Plug>Sneak_t", { noremap = true })
+            vim.keymap.set('n', 'T', "<Plug>Sneak_T", { noremap = true })
+        end
+    },
+    {
+        "nvim-mini/mini.pairs",
+        version = false,
+        opts = {}
+    },
+    {
+        "kylechui/nvim-surround",
+        event = "VeryLazy",
+        opts = {},
+    },
+    {
+        'nvim-mini/mini.comment',
+        version = false,
+        opts = {},
+    },
+    {
+        'nvim-mini/mini.cursorword',
+        version = false,
+        opts = {},
+    },
+}
--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -51,22 +51,6 @@ return {
         },
         opts = {},
     },
-    {
-        "justinmk/vim-sneak",
-        lazy = false,
-        init = function()
-            vim.g["sneak#label"] = 1
-            vim.g["sneak#s_next"] = 1
-        end,
-        config = function()
-            vim.api.nvim_set_hl(0, "Sneak", { link = "IncSearch" })
-
-            vim.keymap.set('n', 'f', "<Plug>Sneak_f", { noremap = true })
-            vim.keymap.set('n', 'F', "<Plug>Sneak_F", { noremap = true })
-            vim.keymap.set('n', 't', "<Plug>Sneak_t", { noremap = true })
-            vim.keymap.set('n', 'T', "<Plug>Sneak_T", { noremap = true })
-        end
-    },
 
     -- FOLDS
     {
