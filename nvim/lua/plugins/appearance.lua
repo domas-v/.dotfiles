a7ed9bd4fc7c2de0e7fe3d88a508f82bb7de2cf4
Dom <domas.vaitmonas93@gmail.com>
2026-06-15 22:51:53 +0300

    kanagawa

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -37,6 +37,29 @@ return {
         end
     },
     {
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('kanagawa').setup({
+                compile = false,
+                undercurl = true,
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { bold = true },
+                statementStyle = { bold = true },
+                terminalColors = true,
+                theme = "wave",
+                background = {
+                    dark = "wave",
+                    light = "lotus",
+                },
+                colors = { theme = { all = { ui = { bg_gutter = "none" } } } }
+            })
+        end
+    },
+    -- setup must be called before loading
+    {
         "shortcuts/no-neck-pain.nvim",
         version = "*",
         config = function()
