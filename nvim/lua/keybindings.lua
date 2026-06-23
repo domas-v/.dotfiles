b29459fa1a39dbbade7f27c5b5ff79dda084d9dd
Dom <domas.vaitmonas93@gmail.com>
2026-06-23 15:36:27 +0300

    change keybinds of tabs

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -62,10 +62,8 @@ vim.keymap.set("n", "<esc>", "<cmd>nohl<cr>", { noremap = true })
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
 
 -- tabs
-vim.keymap.set("n", "<leader>tt", "<cmd>tab split<cr>", { noremap = true })
-vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<cr>", { noremap = true })
-vim.keymap.set("n", "<leader>tn", "<cmd>tabnext<cr>", { noremap = true })
-vim.keymap.set("n", "<leader>tp", "<cmd>tabprevious<cr>", { noremap = true })
+vim.keymap.set("n", "<leader>T", "<cmd>tab split<cr>", { noremap = true })
+vim.keymap.set("n", "<leader>X", "<cmd>tabclose<cr>", { noremap = true })
 
 -- buffers
 vim.keymap.set("n", "<C-,>", "<C-6>", default_opts)
@@ -76,12 +74,6 @@ vim.keymap.set("n", "<leader>C", "<cmd>copen<cr>", { noremap = true })
 vim.keymap.set("n", "<leader>L", "<cmd>lopen<cr>", { noremap = true })
 vim.keymap.set("n", "<leader>ca", "<cmd>caddexpr expand('%') . ':' . line('.') . ':' . getline('.')<cr>",
     { noremap = true, silent = true, desc = "Add current line to quickfix list" })
-
--- -- resize splits
--- vim.keymap.set("n", "<C-->", "<C-w>10<", default_opts)
--- vim.keymap.set("n", "<C-=>", "<C-w>10>", default_opts)
--- vim.keymap.set("n", "<C-S-->", "5<C-W>-", default_opts)
--- vim.keymap.set("n", "<C-S-=>", "5<C-W>+", default_opts)
 
 -- resize splits
 vim.keymap.set("n", "+", "<cmd>vertical resize +10<cr>", default_opts)
